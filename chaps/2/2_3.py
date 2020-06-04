a01564b4cb76d15d717cf1821dc5a848bc07490e
ma <ma_yamaki@yahoo.com>
2020-06-05 04:54:10 +0900

    mmm

--- chaps/2/2_3.py
+++ chaps/2/2_3.py
@@ -6,13 +6,38 @@ plt.style.use('seaborn-darkgrid')
 
 
 
-def metropolis(func, steps=10000):
-    
+def metropolis(func, steps=1000):
+    samples = np.zeros(steps)
+    old_x = func.mean()
+    old_prob = func.pdf(old_x)
 
-points = 15
-h, n = 1, 4
-grid, posterior = posterior_grid(points, h, n)
-plt.plot(grid, posterior, 'o-', label='heads = {}\ntossess = {}'.format(h, n))
-plt.xlabel(r'#\theta$', fontsize=14)
-plt.legend(loc=0, fontsize=16)
-plt.savefig('img21.png')
+    for i in range(steps):
+        new_x = old_x + np.random.normal(0, 0.5)
+        new_prob = func.pdf(new_x)
+        acceptance = new_prob / old_prob
+
+        if acceptance >= np.random.random():
+            samples[i] = new_x
+            old_x = new_x
+            old_prob = new_prob
+        else:
+            samples[i] = old_x
+    return samples
+
+
+
+
+
+
+func = stats.beta(0.4, 2)
+samples = metropolis(func=func)
+x = np.linspace(0.01, 0.99, 100)
+y = func.pdf(x)
+
+plt.plot(x, y, 'r-', lw=3, label='True distribution')
+plt.hist(samples, bins=30, density=True, label='Estimated distributuion')
+plt.xlabel('$x$', fontsize=14)
+plt.ylabel('$pdf(x)$', fontsize=14)
+plt.legend(fontsize=14)
+plt.savefig('img204.png')
+print("fin")
