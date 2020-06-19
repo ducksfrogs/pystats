7623040aa0fd796bb6be98cb31556830e7b44a99
ma <ma_yamaki@yahoo.com>
2020-06-19 15:41:36 +0900

    t-test2

--- lazy_t/t-test.py
+++ lazy_t/t-test.py
@@ -8,4 +8,10 @@ b = np.random.randn(N)
 var_a = a.var(ddof=1)
 var_b = b.var(ddof=1)
 s = np.sqrt((var_a + var_b) /2)
-t = (a.mean() - b.mean()) /(s* np.sqrt(2.9/N))
+t = (a.mean() - b.mean()) /(s* np.sqrt(2.0/N))
+df = 2*N -2
+p = 1 - stats.t.cdf(t,df=df)
+print("t:", t ,p)
+
+t2,p2 =stats.ttest_ind(a,b)
+print(t2, p2)
