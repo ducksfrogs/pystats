05c04d69311b8d56552dd6fa37346e93d8edc1f0
ducksfrogs <ma_yamaki@yahoo.com>
2021-06-13 21:14:48 +0900

    mm

---
+++ mom/code/future.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 100)
+
+from itertools import product
+from sklearn.preprocessing import LabelEncoder
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+
+
+def plot_feature(booster, figsize):
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    return plot_importance(booster=booster, ax=ax)
+
+import time
+import sys
+import gc
+import pickle
+
+items = pd.read_csv("../input/items.csv")
+shops = pd.read_csv('../input/shops.csv')
+cats = pd.read_csv('../input/item_categories.csv')
+train = pd.read_csv('../input/sales_train.csv')
+test = pd.read_csv('../input/test.csv').set_index('ID')
