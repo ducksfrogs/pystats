65af1ab936ea2127ae43751300185920567eb91a
ducksfrogs <ma_yamaki@yahoo.com>
2022-01-31 19:02:44 +0900

    d

---
+++ schit/titanic.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import random as rd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+train_df = pd.read_csv('../data/train.csv')
+test_df = pd.read_csv('../data/test.csv')
+
+combine = [train_df, test_df]
+
+
+train_df.info()
+print("_"*50)
+test_df.info()
